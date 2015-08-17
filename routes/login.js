3c28548ede5ec2726e7a1f011fe76c300c94848a
pratyushajo <pjoginipally@scu.edu>
2015-08-16 20:01:37 -0700

    Connecting to DB for login
    Created a table users and doing authentication

--- routes/login.js
+++ routes/login.js
@@ -8,4 +8,39 @@ router.get('/', function(req, res, next) {
 	res.render('login');
 });
 
+var getLoginCredentials = function(req, res, next) {
+	var username = req.params;
+	var password = req.body.password;
+	console.log(username);
+}
+
+router.post('/login',function(req,res){
+	console.log(req.body) //you will get your data in this as object.
+	var username = req.body.username;
+	var password = req.body.password;
+	
+	pg.connect(process.env.DATABASE_URL, function(err, client) {
+	    if (!err) {
+	      // Check if username and password match
+	      var query = client.query("SELECT password FROM users WHERE username= '" + username + "'", function(err, result) {
+	        if (!err) {
+	          if (result.rows.length == 0) {
+	            sys.log("No user found");
+	          } else {
+	            sys.log("user found");
+	            res.render('home');
+	          }
+	        } else {
+	          sys.log("login: Error querying DB " + err);
+	        }
+
+	        client.end();
+	      });
+      } else {
+        sys.log("login: Error connecting to DB, " + err);
+      }
+    });
+   
+})
+
 module.exports = router;
